dc9842faade215f32682195b4d9729b6d2947a7b
Artiom FEDOROV <fedorov.artiom@gmail.com>
2017-01-15 23:56:46 +0100

    IR device config

--- src/physical_device/UsbInfraRedDevice.ts
+++ src/physical_device/UsbInfraRedDevice.ts
@@ -1,3 +1,6 @@
+declare var require: any
+var cmd = require('node-cmd')
+
 export class UsbInfraRedDevice {
 
     private device_path:string
@@ -9,8 +12,19 @@ export class UsbInfraRedDevice {
     public sendIrMessage(ir_message: string) {
         // echo "Message test">/dev/ttyACM0
         // echo $((16#FF00FF88)) > /dev/ttyACM0
-        let cmd = 'echo ""' + ir_message + '"" > ' + this.device_path
+        // echo $((16#FFF807)) > /dev/ttyACM0
+        let cmd = 'echo $((16#' + ir_message + ')) > ' + this.device_path
+        this.executeCmd(cmd)
         console.log(cmd)
     }
+
+    public executeCmd(cmd_string: string) {
+        cmd.run(cmd_string)
+        /*
+        cmd.run(cmd_string, function(data: any){
+            console.log('the current working dir is : ', data)
+        });*/
+    }
+
 
 }
